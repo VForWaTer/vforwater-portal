55a377e51ac25db9846357ab8e82008adc71626d
Florian Klemme <mail@florianklemme.de>
2016-12-08 17:57:20 +0100

    Use local database for now. No Postgis required.

--- heron/settings.py
+++ heron/settings.py
@@ -76,23 +76,30 @@ WSGI_APPLICATION = 'heron.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.contrib.gis.db.backends.postgis',
+#         'NAME': 'vforwater',
+#         'USER': 'postgres',
+#         'PASSWORD': 'vforwater',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#         'OPTIONS': {
+#             'options': '-c search_path=data,meta_catalog,public'
+#         }
+# #        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+# #        'NAME': 'vforwater_update',
+# #        'USER': 'postgres',
+# #        'PASSWORD': 'vforwater',
+# #        'HOST': 'localhost',
+# #        'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'vforwater',
-        'USER': 'postgres',
-        'PASSWORD': 'vforwater',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        'OPTIONS': {
-            'options': '-c search_path=data,meta_catalog,public'
-        }
-#        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-#        'NAME': 'vforwater_update',
-#        'USER': 'postgres',
-#        'PASSWORD': 'vforwater',
-#        'HOST': 'localhost',
-#        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 
