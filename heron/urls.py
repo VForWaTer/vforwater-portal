87483c7406c6882645ca24c495faf60e276cd379
Florian Klemme <mail@florianklemme.de>
2016-12-21 10:56:23 +0100

    Fixed minor warning.

--- heron/urls.py
+++ heron/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('vfwheron.urls')),
+    url(r'^', include('vfwheron.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^heron_wps/', include('heron_wps.urls', namespace='heron_wps')),
 ]
