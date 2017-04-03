2c3976a37743daddc0dff961813ea2d100cdc38e
marcus <marcus@MarxKitBook.scc.kit.edu>
2017-04-03 09:04:59 +0200

    views angepasst an die nicht default database

--- vfwheron/views.py
+++ vfwheron/views.py
@@ -7,7 +7,7 @@ class HomeView(TemplateView):
     template_name = 'vfwheron/home.html'
 
     def get_context_data(self, **kwargs):
-        all_variable_names = TblVariable.objects.all() 
+        all_variable_names = TblVariable.objects.using('vforwater').all()
         return {'all_names': all_variable_names} 
     
 class ExtlinksView(TemplateView):
