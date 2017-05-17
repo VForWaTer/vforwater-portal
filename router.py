1711cbc26fd5685b7fd5abe187281471b9a23441
Florian Klemme <mail@florianklemme.de>
2017-05-17 17:29:46 +0200

    Fixed resolution of target migration database.

--- router.py
+++ router.py
@@ -16,22 +16,21 @@ class VforwaterRouter(object):
         return None
 
     def allow_relation(self, obj1, obj2, **hints):
-
+        # TODO: Is such a stringent relationship required? / Where is this example from? (FK)
         # Allow any relation between two models that are both in the vfwheron app.
         if obj1._meta.app_label == 'vfwheron' and obj2._meta.app_label == 'vfwheron':
             return True
-        # No opinion if neither object is in the Example app (defer to default or other routers).
+        # No opinion if neither object is in the vfwheron app (defer to default or other routers).
         elif 'vfwheron' not in [obj1._meta.app_label, obj2._meta.app_label]:
             return None
 
-        # Block relationship if one object is in the Example app and the other isn't.
+        # Block relationship if one object is in the vfwheron app and the other isn't.
         return False
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
-        """Ensure that the app's models get created on the right database."""
+        """Ensure that vfwheron's models get created on the PostGIS database."""
         if app_label == 'vfwheron':
-            # The Example app should be migrated only on the example_db database.
             return db == 'vforwater'
 
-        # No opinion for all other scenarios
-        return None
+        # All others are created on the default database.
+        return db == 'default'
