644e6053358de7cd63414168c56086fb3c5dce0b
marcus <marcus@MarxKitBook.scc.kit.edu>
2017-09-03 14:29:55 +0200

    started to set up a menu

--- vfwheron/query_functions.py
+++ vfwheron/query_functions.py
@@ -5,24 +5,57 @@ from django.db import connections
 from vfwheron.models import TblVariable
 
 
-def get_bbox_from_data():
-    # get bbox for available data
+def get_bbox_from_data(): # get bbox for available data
     cursor = connections['vforwater'].cursor() # connect to database
-    # request bbox in srid of openlayers
+    # request bbox in srid of openlayers:
     cursor.execute('SELECT ST_Extent(ST_Transform(ST_SetSRID(ST_Point(centroid_x, centroid_y),srid),3857)) FROM lt_location;')
     m = re.findall("(\d+.\d*)", cursor.fetchall()[0][0]) # get string with coordinates
-    dataExt = list(map(lambda x: float(x), m)) # change string to list of floats
-    return dataExt
+    return list(map(lambda x: float(x), m)) # change string to list of floats 
 
 def get_filter_values():
     start_values = 0
     return start_values
 
 def get_first_level():
-    first_level = level_one_tables = {'variable':'Datentyp', 'soil':'Boden', 'site':'Standort','publisher':'Besitzer',
+    first_level =  {'variable':'Datentyp', 'soil':'Boden', 'site':'Standort','publisher':'Besitzer',
     'nmmetadomain':'Domäne'}
     return first_level
 
-def second_filter_level():
-    variable = TblVariable.objects.select_related('unit').values('variable_name', 'variable_symbol', 'unit__unit_symbol')
-    return variable
+def get_second_level():
+    vBodGeo = 0
+    vBodTyp = 0
+    vBesIns = 0
+    vBesNam = 0
+    vBesDep = 0
+    vDomPrj = 0
+    vDomDom = 0
+    vStaNam = 0
+    vDatNam = 0
+    # second_level = [{'Boden': ['Geologie', 'Bodentyp']}, {'Besitzer': ['Institution Name', 'Nachname', 'Department']},
+    #     {'Domäne': ['Projekt', 'Name der Domäne']}, {'Standort': ['Standort Name']},{'Datentyp': ['Name der Variable']}]
+    second_level = [{'Boden': [{'Geologie':vBodGeo}, {'Bodentyp':vBodTyp}]}, {'Besitzer': [{'Institution Name':vBesIns},
+        {'Nachname':vBesNam},{'Department':vBesDep}]}]
+    # second_level = [{'Boden': [{'Geologie':vBodGeo}, {'Bodentyp':vBodTyp}]}, {'Besitzer': [{'Institution Name':vBesIns},
+    #     {'Nachname':vBesNam},{'Department':vBesDep}]},{'Domäne': [{'Projekt':vDomPrj},{'Name der Domäne':vDomDom}]},
+    #     {'Standort': [{'Standort Name':vStaNam}]},{'Datentyp': [{'Name der Variable':vDatNam}]}]
+    return second_level
+
+def get_menu_names():
+    menu_names = {'Boden':'Geologie', 'Boden':'Bodentyp', 'publisher__institution_name':'Institution Name', 
+        'publisher__last_name':'Nachname', 'publisher__department':'Department',
+       'nmmetadomain__domain__project__project_name':'Projekt', 'site__site_name':'Standort Name', 
+       'variable__variable_name':'Name der Variable', 'nmmetadomain__domain__domain_name':'Name der Domäne'}
+    return menu_names
+    
+class menu_titles(object):
+    def __init__(self, first_level, first_level_link, second_level, second_level_link):
+        self.first_level = first_level
+        self.first_level_link = first_level_link
+        self.second_level = second_level
+        self.second_level_link = second_level_link
+
+    # first_level = ''
+    # first_level_link = ''
+    # second_level = ''
+    # second_level_link = ''
+
--- vfwheron/views.py
+++ vfwheron/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import redirect
 from django.apps import apps
 from vfwheron.models import TblVariable, LtLocation, LtProject, LtDomain, LtLicense, TblMeta
 
-from vfwheron.query_functions import get_bbox_from_data, get_filter_values, second_filter_level, get_first_level
+from vfwheron.query_functions import get_bbox_from_data, get_filter_values, get_first_level, get_second_level
 
 import logging
 
@@ -19,59 +19,68 @@ class HomeView(TemplateView):
     template_name = 'vfwheron/home.html'
 
     my_app = apps.get_app_config('vfwheron')
-    print('----------', my_app.models)
+    # print('----------', my_app.models)
     all_tables = [m._meta.object_name for m in my_app.models.values()]
-    print('##classes: ', all_tables)
+    # print('##classes: ', all_tables)
     unused_tables = ['Basiseinzugsgebiet', 'NmMetaDomain', 'DjangoMigrations', 'SpatialRefSys', 'TblMeta',
                      'LtQuality', 'LtLicense', 'LtUnit']
     level_one_tables_sql = list(set(all_tables) - set(unused_tables))
-    print('level_one_tables_sql: ', level_one_tables_sql)
+    # print('level_one_tables_sql: ', level_one_tables_sql)
 
     level_one_tables = {'variable':'Datentyp', 'soil':'Boden', 'site':'Standort','publisher':'Besitzer',
-    'nmmetadomain':'Domäne'}
-    print('level_one_tables: ', level_one_tables)
+                        'nmmetadomain':'Domäne'}
+    # print('level_one_tables: ', level_one_tables)
 
-    print('get_filter_values: ', get_filter_values())
-
-    for m in my_app.models:
-        print('keys: ', m, ' | values: ', my_app.models[m])
+    # print('get_filter_values: ', get_filter_values())
+    # print('my app: ', my_app.models)
+    # for m in my_app.models:
+    #     print('keys: ', m, ' | values: ', my_app.models[m])
 
     # print('#+#+#: ', my_app.models['tblvariable'].objects.select_related('unit').values_list('variable_name',
     # 'variable_symbol'))
-    print('**domain_name**', LtDomain.objects.select_related('project').values('domain_name').distinct)
-    print('+TblVariable+',TblVariable.objects.select_related('unit').values('variable_name', 'variable_symbol','unit__unit_symbol').distinct)
-    print('+LtProject+', LtProject.objects.select_related('project_name').values('project_name').distinct)
+    # print('**domain_name**', LtDomain.objects.select_related('project').values('domain_name').distinct)
+    # print('+TblVariable+',TblVariable.objects.select_related('unit').values('variable_name', 'variable_symbol','unit__unit_symbol').distinct)
+    # print('+LtProject+', LtProject.objects.select_related('project_name').values('project_name').distinct)
 
     # print('****', LtLicense.objects.select_related('license_abbrev').distinct()) // geht nix
-    print('****', LtLocation.objects.select_related('srid').distinct())
+    # print('****', LtLocation.objects.select_related('srid').distinct())
 
-    print(' m e t a : ', TblMeta.objects.select_related('creator', 'publisher', 'geometry',
-        'license', 'quality','site', 'soil', 'variable', 'sensor', 'source').all())
+    # print(' m e t a : ', TblMeta.objects.select_related('creator', 'publisher', 'geometry',
+    #                                                     'license', 'quality','site', 'soil', 'variable', 'sensor', 'source').all())
 
 
     # metaquest = TblMeta.objects.using('vforwater').select_related('creator__last_name', 'publisher__last_name', 'geometry__srid',
     #     'license', 'quality','site', 'soil', 'variable', 'sensor', 'source').all()
 
-    filterworthy_values = ['variable__variable_name', 'soil__geology', 'soil__soil_type',
-        'site__site_name','publisher__last_name', 'publisher__institution_name', 'publisher__department',
-        'nmmetadomain__domain__domain_name', 'nmmetadomain__domain__project__project_name']
-    print('filterworthy_values: ',filterworthy_values)
+    # filterworthy_values = ['variable__variable_name', 'soil__geology', 'soil__soil_type',
+    #                        'site__site_name','publisher__last_name', 'publisher__institution_name', 'publisher__department',
+    #                        'nmmetadomain__domain__domain_name', 'nmmetadomain__domain__project__project_name']
+    # print('filterworthy_values: ',filterworthy_values)
 
-    metaquest = TblMeta.objects.filter(variable__variable_name='Lufttemperatur',soil__geology = 'marls', site__landuse = 'forest' ).\
-        values(*filterworthy_values).distinct()
-    print('######## # # # # ',metaquest)
-
-    metaquest = TblMeta.objects.filter(variable__variable_name='Lufttemperatur',soil__geology = 'marls', site__landuse = 'forest' ).\
-        values(*filterworthy_values)
-    print('######## # # # # ',metaquest)
-    print('## ## ## M e T a : ', [m for m in metaquest.values('variable__variable_name', 'source__source_type')] )
+    # metaquest = TblMeta.objects.filter(variable__variable_name='Lufttemperatur',soil__geology = 'marls', site__landuse = 'forest' ). \
+    #     values(*filterworthy_values).distinct()
+    # # print('######## # # # # ',metaquest)
+    #
+    # metaquest = TblMeta.objects.filter(variable__variable_name='Lufttemperatur',soil__geology = 'marls', site__landuse = 'forest' ). \
+    #     values(*filterworthy_values)
+    # print('######## # # # # ',metaquest)
+    # print('## ## ## M e T a : ', [m for m in metaquest.values('variable__variable_name', 'source__source_type')] )
 
     # print('second_filter_level: ', second_filter_level())
+    for m in get_second_level():
+        print('***, m: ', m)
+        print('1, m.keys(): ', m.keys())
+        print('2, m.values(): ', m.values())
+        for n in m.values():
+            print('3, n: ', n)
+            print('4, n[0]: ', n[0])
+            for o in n:
+                print ('5:', o.keys(), o.values())
 
     def get_context_data(self, **kwargs):
-        get_unit_id = TblVariable.objects.select_related('variable_id').values_list('variable__name')
+        get_unit_id = TblVariable.objects.select_related('unit').values_list('variable_name', 'variable_symbol')
         all_variable_names = get_unit_id.values('variable_name', 'variable_symbol', 'unit__unit_symbol')
-        return {'dataExt': get_bbox_from_data(), 'first_level':get_first_level(), 'all_names': all_variable_names}
+        return {'dataExt': get_bbox_from_data(), 'first_level':get_first_level(), 'menue_list':get_second_level(), 'all_names': all_variable_names}
 
 
 class ExtlinksView(TemplateView):
