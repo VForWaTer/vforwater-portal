b971deb1e2b9ad8df28d686b73942d4325dde9ee
marcus <marcus@MarxKitBook.scc.kit.edu>
2017-09-26 13:13:18 +0200

    display data points

--- vfwheron/static/vfwheron/js/map.js
+++ vfwheron/static/vfwheron/js/map.js
@@ -3,34 +3,7 @@ function create_map() {
 	mapsource = new ol.source.XYZ({url: window.location.origin + "/osm/{z}/{x}/{y}.png"})
 	
 	var dataExt = JSON.parse(document.getElementById('dataExt').value); // bbox of available data
-/*    var sample_locations = document.getElementById('sample_locations').value;
-    console.log(sample_locations);
 
-// Build a vector layer:
-    var image = new ol.style.Circle({
-        radius: 5,
-        fill: null,
-        stroke: new ol.style.Stroke({color: 'blue', width: 1})
-    });
-
-    var styles = {
-        'Point': new ol.style.Style({
-          image: image
-        }),
-    }
-
-    var styleFunction = function(feature) {
-        return styles[feature.getGeometry().getType()];
-      };
-
-    var vectorSource = new ol.source.Vector({
-        features: (new ol.format.GeoJSON()).readFeatures(sample_locations)
-      });
-
-    var vectorLayer = new ol.layer.Vector({
-        source: vectorSource,
-        style: styleFunction
-      });*/
 
 // build the background map
 	maplayer = new ol.layer.Tile({
@@ -47,36 +20,22 @@ function create_map() {
 	mapview = new ol.View({
 		center: ol.proj.fromLonLat([11.8810049, 50.0836865]),
 		zoom: 6,
-		maxZoom: 18,
+		maxZoom: 20,
 	});
 
-//	Access as wfs - not used now
-/*		vectorSource = new ol.source.Vector({
-              format: new ol.format.GeoJSON({defaultDataProjection:"ESPG:4326"}),
-                url: function(extent) {
-              return 'https://vforwater-gis.scc.kit.edu/geoserver/wfs?service=WFS&' +
-              'version=2.0.0&request=GetFeature&typename=LUBW:vfwheron_basiseinzugsgebiet&' +
-              'outputFormat=application/json&' +
-              'bbox=' + extent.join(',') + ',EPSG:4326';
-        },
-        strategy: ol.loadingstrategy.bbox
-                });
 
-                  vectormap = new ol.layer.Vector({
-                  source: vectorSource,
-                  style: new ol.style.Style({
-                    stroke: new ol.style.Stroke({
-                      color: 'rgba(0, 100, 255, 1.0)',
-                      width: 1.5
-                    })
-                  })
-                });
- */
+    pointsource = new ol.source.TileWMS({
+		url: 'https://vforwater-gis.scc.kit.edu/geoserver/wms',
+		params: {LAYERS: 'CAOS:lt_location'}
+	});
+	pointmap = new ol.layer.Tile({
+		source: pointsource,
+	});
+
 	vectorSource = new ol.source.TileWMS({
 		url: 'https://vforwater-gis.scc.kit.edu/geoserver/wms',
 		params: {LAYERS: 'LUBW:vfwheron_basiseinzugsgebiet'}
 	});
-
 	vectormap = new ol.layer.Tile({
 		source: vectorSource,
 	});
@@ -86,7 +45,7 @@ function create_map() {
 	map = new ol.Map({
 		renderer: 'canvas',
 		target: map_tar,
-		layers: [maplayer, vectormap],
+		layers: [maplayer, vectormap, pointmap],
 		controls: [
 			new ol.control.Zoom(),
 			new ol.control.Attribution(),
