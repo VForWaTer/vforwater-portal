10fcf555c6525a53249d1b13aff7d06a9767590a
marcus <marcus.strobl@kit.edu>
2017-11-03 13:50:30 +0100

    try to get hover funtion running 3

--- vfwheron/static/vfwheron/js/map.js
+++ vfwheron/static/vfwheron/js/map.js
@@ -205,9 +205,8 @@ function create_map() {
       return;
     }
     var pixel = map.getEventPixel(evt.originalEvent);
-    var hit = map.forEachLayerAtPixel(pixel, { layerFilter: function (layer) {
+    var hit = map.forEachLayerAtPixel(pixel, function layerFilter(layer) {
       return layer.get('name') === 'WMSpointSource';
-      }
     });
     map.getTargetElement().style.cursor = hit ? 'pointer' : '';
   });
