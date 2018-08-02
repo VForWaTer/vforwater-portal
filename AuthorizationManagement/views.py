6deb8b2aa4b5a5e6670b5742202b0f53bff769c1
marcus <marcus@MarxKitBook.scc.kit.edu>
2018-08-02 09:11:14 +0200

    Remove depreciated login function from urls and exchange with LoginView; Necessary for Django 2.1.

--- AuthorizationManagement/views.py
+++ AuthorizationManagement/views.py
@@ -19,7 +19,7 @@ from django.core.exceptions import PermissionDenied, ValidationError
 from _csv import reader, Error
 import mimetypes
 from test.support import resource
-from pip._vendor.requests.api import request
+# from pip._vendor.requests.api import request  # TODO: what is this? It's whether used nor available.
 from django.db.models import Q
 from django.views.decorators.cache import never_cache
 from django.shortcuts import get_object_or_404
