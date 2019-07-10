35d7da6b98c09dc1022c43403e4c0e3e38092ff4
Joerg Meyer <joerg.meyer2@kit.edu>
2019-07-10 13:24:06 +0200

    comment out unused stuff

--- heron/urls.py
+++ heron/urls.py
@@ -25,7 +25,7 @@ print('   *   heron urls.py')
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url='vfwheron/', permanent=False)),
     url(r'^vfwheron/', include('vfwheron.urls')),
-    url(r'^dashboard/', include('dashboard.urls')),
+#    url(r'^dashboard/', include('dashboard.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^heron_wps/', include('heron_wps.urls', namespace='heron_wps')),
     url(r'^heron_monitor/', include('heron_monitor.urls', namespace='heron_monitor')),
