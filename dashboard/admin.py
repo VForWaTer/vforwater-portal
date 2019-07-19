abe4e6e7b7c5d60e7442e2dcc842d1ed73b498fe
marcus <marcus@MarxKitBook.scc.kit.edu>
2019-07-19 16:24:57 +0200

    removal of some unused code

--- dashboard/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import WebProcessingService
-
-admin.site.register(WebProcessingService)
--- dashboard/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class WebProcessingService(models.Model):
-    """
-    
-    """
-    name = models.CharField(max_length=100)
-    uri = models.CharField(max_length=250)
