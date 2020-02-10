7a1a98c25db3b7cfcd0f9a404c90d7749457ad2b
marcus <fq3407@MarxKitBook.scc.kit.edu>
2020-02-10 14:28:00 +0100

    Add simple Windrose for preview of direction data

--- vfwheron/previewplot.py
+++ vfwheron/previewplot.py
@@ -2,95 +2,32 @@
 
 """
 
-import base64
 from datetime import time, datetime, timezone, timedelta
+from math import radians, ceil, sqrt
 
-import matplotlib as mpl
 from bokeh.layouts import column
-from bokeh.models import Band, DatetimeTickFormatter, HoverTool, VBar, LinearAxis, Range1d
+from bokeh.models import Band, DatetimeTickFormatter, HoverTool, AnnularWedge, LinearAxis, Grid, Plot, Range1d
 from bokeh.transform import linear_cmap
+from bokeh.plotting import figure
+from bokeh.models import ColumnDataSource
+from bokeh.embed import components
+from bokeh.palettes import Oranges9
+
 from django.db import connections
-from io import BytesIO
 
 from vfwheron.models import TblMeta, TblData
-# mpl.use('Agg')
-import matplotlib.pyplot as plt
 
 import redis
 
 
-# def get_preview(preview):
-#     """
-#
-#     :param preview:
-#     :type preview:
-#     :return:
-#     :rtype:
-#     """
-#     use_redis = True
-#     in_cache = False
-#     try:
-#         r = redis.StrictRedis()
-#         b64 = r.get("preview_{}".format(preview))
-#     except:
-#         use_redis = False
-#     if use_redis:
-#         if b64 is None:
-#             in_cache = False
-#         else:
-#             b64 = str(b64, 'utf-8')
-#             in_cache = True
-#
-#     if not in_cache:
-#         # preview = 1157
-#         label = TblMeta.objects.filter(id=preview).values_list('variable__variable_name',
-#                                                                'variable__variable_symbol',
-#                                                                'variable__unit__unit_abbrev')
-#         ylabel = label[0][0] + ' (' + label[0][1] + ')' + ' [' + label[0][2] + ']'
-#         # connect to database
-#         cursor = connections['default'].cursor()
-#         cursor.execute(
-#                 "SELECT date_trunc('day', tstamp) as date, avg(value) as avg, "
-#                 "min(value) as min, max(value) as max "
-#                 "FROM tbl_data WHERE meta_id = %s GROUP BY date_trunc('day', tstamp);" % preview)
-#         m = cursor.fetchall()
-#         cursor.close()
-#
-#         x = [row[0] for row in m]
-#         yavg = [row[1] for row in m]
-#         ymin = [row[2] for row in m]
-#         ymax = [row[3] for row in m]
-#
-#         fig, ax = plt.subplots(1, 1, figsize=(6, 4))
-#         ax.plot(x, ymin, '-c', x, ymax, '-c', lw=0.3, label='Daily min/max')
-#         ax.plot(x, yavg, '-b', lw=1, label="Daily average")
-#         fig.autofmt_xdate(),
-#         ax.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
-#         ax.set_xlabel('Date')
-#         ax.grid(which='major', axis='x')
-#         ax.set_ylabel(ylabel)
-#         ax.set_title('Dataset ' + str(preview))
-#         # create tempfile and read as base64
-#         tmpfile = BytesIO()
-#         fig.savefig(tmpfile, format='png')
-#         tmpfile.seek(0)
-#         b64 = base64.b64encode(tmpfile.getvalue()).decode('utf8')
-#         if use_redis:
-#             r.set("preview_{}".format(preview), b64)
-#
-#             # create the image-tag
-#     imgtag = "<img alt='data image' src='data:image/png;base64,%s'>" % b64
-#     return str(imgtag)
-
-
-def DB_load(ID):
+def DB_load_label(ID):
     label = TblMeta.objects.filter(id=ID).values_list('variable__variable_name',
                                                       'variable__variable_symbol',
                                                       'variable__unit__unit_abbrev')
-    ylabel = label[0][0] + ' (' + label[0][1] + ')' + ' [' + label[0][2] + ']'
-    print('Label: ', label)
-    print('yLabel: ', ylabel)
-    print('yLabel: ', type(ylabel))
+    return label[0][0] + ' (' + label[0][1] + ')' + ' [' + label[0][2] + ']'
+
+
+def DB_load_data(ID):
     # TODO: Use django ORM instead of pure sql
     # connect to database and fetch day(x), daily average, daily min, daily max, # of daily values
     cursor = connections['default'].cursor()
@@ -103,8 +40,119 @@ def DB_load(ID):
     dbresult = cursor.fetchall()
     cursor.close()
     result = list(zip(*dbresult))
+    # y1 is for the main plot -> min and  max of the day of the day,
+    # y2 for the secondary plot with min and max in each group for each day
     axis = {'y1min': min(result[2]), 'y1max': max(result[3]), 'y2min': min(result[4]), 'y2max': max(result[4])}
-    return {'data': result, 'ylabel': ylabel, 'axis': axis}
+    return {'data': result, 'axis': axis}
+
+
+def get_bokeh_standard(DBdata, label):
+    source = ColumnDataSource({'date': DBdata['data'][0], 'y': DBdata['data'][1],
+                               'ymin': DBdata['data'][2], 'ymax': DBdata['data'][3],
+                               'count': DBdata['data'][4]})
+    # Plot average as main plot
+    mainplot = figure(title='Daily average, min and max values', x_axis_label='Time', x_axis_type="datetime",
+                      y_axis_label=label,
+                      plot_width=700, plot_height=500, toolbar_location="above",
+                      tools="pan,wheel_zoom,box_zoom,reset", active_drag="box_zoom")
+
+    # plot.toolbar.autohide = True
+    # plot average line
+    mainplot.line(x='date', y='y', source=source, line_width=2, legend_label="average")
+
+    # TODO: Figure out how to use 'source' for multi_line.
+    #  Maybe use Glyph? (https://docs.bokeh.org/en/latest/docs/reference/models/glyphs/multi_line.html)
+    #  Glyphs maybe also helpful for hover_tool on multiline?
+    # plot.add_tools(HoverTool(tooltips=[("value", "$y"), ("Date", "@date{%d %b %Y}")], formatters={"date":
+    # "datetime"}, mode="mouse"))
+
+    mainplot.add_tools(HoverTool(tooltips=[("value at pointer", "$y")], mode="mouse"))
+
+    # plot min/max as multiline and fill area with band
+    mainplot.multi_line(xs=[DBdata['data'][0], DBdata['data'][0]],
+                        ys=[DBdata['data'][2], DBdata['data'][3]], level='underlay',
+                        color=['lightblue', 'lightblue'], legend_label="min & max values")
+    mainplot.add_layout(Band(base='date', lower='ymin', upper='ymax', source=source, level='underlay',
+                             fill_color='lightblue', fill_alpha=0.5))
+
+    # plot bars for the number of values in each group as secondary 'by'plot
+    mapper = linear_cmap(field_name='count', palette=Oranges9, low=0, high=DBdata['axis']['y2max'])
+    width = DBdata['data'][0][1] - DBdata['data'][0][0]
+    byplot = figure(title='Number of available values per day', x_axis_type="datetime", x_range=mainplot.x_range,
+                    plot_width=700, plot_height=50, toolbar_location=None, background_fill_color="black")
+    byplot.vbar(x='date', source=source, width=width, bottom=0, top=1, color=mapper)
+    # byplot.vbar(x='date', source=source, width=width, bottom=0, top='count', color=mapper)
+    byplot.xaxis.visible = False
+    byplot.xgrid.visible = False
+    byplot.yaxis.visible = False
+    byplot.ygrid.visible = False
+    byplot.add_tools(HoverTool(tooltips=[("# of values", "@count")], mode="mouse"))
+
+    # Style the plot
+    mainplot.title.text_font_size = "14pt"
+    mainplot.xaxis.axis_label_text_font_size = "14pt"
+    mainplot.xaxis.formatter = DatetimeTickFormatter(days=["%d %b %Y"], months=["%d %b %Y"], years=["%d %b %Y"])
+    mainplot.yaxis.axis_label_text_font_size = "14pt"
+
+    script, div = components(column(byplot, mainplot), wrap_script=False)
+    return {'script': script, 'div': div}
+
+
+def get_bokeh_direction(DBdata, label):
+    hist = [0] * 36
+    for i in DBdata['data'][1]:
+        hist[i] += 1
+    x = list(range(0, 36))
+    maxhist = max(hist)
+    sumhist = sum(hist)
+    p = figure(plot_height=maxhist, title="Fruit Counts",
+               toolbar_location=None, tools="")
+    p.vbar(x=x, top=hist, width=0.9)
+    p.xgrid.grid_line_color = None
+    p.y_range.start = 0
+
+    p = figure(plot_width=400, plot_height=400, x_axis_type=None, y_axis_type=None,
+               min_border=0, outline_line_color=None)
+    for i in x:
+        p.wedge(radius=hist[i], start_angle=-radians((i * 10) - 95), end_angle=-radians((i * 10) - 85),
+                x=0, y=0, direction='clock')
+    # create grid
+    rund_perc = ceil(maxhist / sumhist * 100)
+    labels = list(range(0, rund_perc, 2))
+    labels.append(rund_perc)
+    bar_val = [i * sumhist / 100 for i in labels]
+    label_pos = [sqrt(((i - 1) ** 2) / 2) for i in bar_val]
+    for rad in bar_val:
+        p.circle(x=0, y=0, radius=rad, fill_color=None, line_color='grey', line_width=0.5, line_alpha=0.8)
+
+    p.text(label_pos[1:], label_pos[1:], [str(r) + ' %' for r in labels[1:]],
+           text_font_size="10pt", text_align="left", text_baseline="top")
+    p.line(x=[0, 0], y=[-maxhist, maxhist], line_color="grey", line_width=0.5, line_alpha=0.8)
+    p.line(y=[0, 0], x=[-maxhist, maxhist], line_color="grey", line_width=0.5, line_alpha=0.8)
+    p.x_range = Range1d(-maxhist, maxhist, bounds=(-1, 2))
+    p.y_range = Range1d(-maxhist, maxhist, bounds=(-1, 2))
+    # script, div = components(column(byplot, mainplot), wrap_script=False)
+    script, div = components(p, wrap_script=False)
+    return {'script': script, 'div': div}
+
+
+def DB_load_directiondata(id):
+    # TODO: Use django ORM instead of pure sql
+    # create 36 groups with group 1 from 355-5 degree and 36 from 345-355 degree
+    cursor = connections['default'].cursor()
+    cursor.execute("SELECT date_trunc('day', tstamp)::date as date, "
+                   "trunc(degrees(atan2(avg(cos(radians(value))), avg(sin(radians(value)))))/10)::smallint as avg, "
+                   "count(*) as amount "
+                   "FROM tbl_data "
+                   "WHERE meta_id = %s "
+                   "GROUP BY date_trunc('day', tstamp) "
+                   "ORDER BY date asc;" % id)
+    dbresult = cursor.fetchall()
+    cursor.close()
+    result = list(zip(*dbresult))
+    axis = {'y1min': min(result[1]), 'y1max': max(result[1])}
+    return {'data': result, 'axis': axis}
+    # return DB_load_data(id)
 
 
 def get_preview(id):
@@ -124,67 +172,16 @@ def get_preview(id):
             in_cache = True
 
     if not in_cache:
-        DBdata = DB_load(id)
+        label = DB_load_label(id)
+        if label.find('direction') != -1:
+            DBdata = DB_load_directiondata(id)
+            # img = get_bokeh_standard(DBdata, label)
+            img = get_bokeh_direction(DBdata, label)
+        else:
+            DBdata = DB_load_data(id)
+            img = get_bokeh_standard(DBdata, label)
 
-        from bokeh.plotting import figure
-        from bokeh.models import ColumnDataSource
-        from bokeh.embed import components
-        source = ColumnDataSource({'date': DBdata['data'][0], 'y': DBdata['data'][1],
-                                   'ymin': DBdata['data'][2], 'ymax': DBdata['data'][3],
-                                   'count': DBdata['data'][4]})
-        # TOOLTIPS = [("date", "@x{%F}"), ("value", "$y")]
-        # formatters={'date': '@date{%F}'}
-
-        # Plot average as main plot
-        mainplot = figure(title='Daily average, min and max values', x_axis_label='Time', x_axis_type="datetime",
-                          y_axis_label=DBdata['ylabel'],
-                      plot_width=700, plot_height=500, toolbar_location="above",
-                      tools="pan,wheel_zoom,box_zoom,reset", active_drag="box_zoom")
-                      # tools="pan,wheel_zoom,box_zoom,reset,crosshair", active_drag="box_zoom", tooltips=TOOLTIPS)
-        # mainplot.y_range = Range1d(round(DBdata['axis']['y1min']), round(DBdata['axis']['y1max']))
-
-        # plot.toolbar.autohide = True
-        # plot average line
-        mainplot.line(x='date', y='y', source=source, line_width=2, legend_label="average")
-
-        # TODO: Figure out how to use 'source' for multi_line.
-        #  Maybe use Glyph? (https://docs.bokeh.org/en/latest/docs/reference/models/glyphs/multi_line.html)
-        #  Glyphs maybe also helpful for hover_tool on multiline?
-        # plot.add_tools(HoverTool(tooltips=[("value", "$y"), ("Date", "@date{%d %b %Y}")], formatters={"date": "datetime"}, mode="mouse"))
-
-        mainplot.add_tools(HoverTool(tooltips=[("value at pointer", "$y")], mode="mouse"))
-
-        # plot min/max as multiline and fill area with band
-        mainplot.multi_line(xs=[DBdata['data'][0], DBdata['data'][0]],
-                            ys=[DBdata['data'][2], DBdata['data'][3]], level='underlay',
-                            color=['lightblue', 'lightblue'], legend_label="min & max values")
-        mainplot.add_layout(Band(base='date', lower='ymin', upper='ymax', source=source, level='underlay',
-                                 fill_color='lightblue', fill_alpha=0.5))
-
-        # plot bars for the number of values in each group as secondary 'by'plot
-        from bokeh.palettes import Oranges9
-        mapper = linear_cmap(field_name='count', palette=Oranges9, low=0, high=DBdata['axis']['y2max'])
-        width = DBdata['data'][0][1] - DBdata['data'][0][0]
-        byplot = figure(title='Number of available values per day', x_axis_type="datetime", x_range=mainplot.x_range,
-                        plot_width=700, plot_height=50, toolbar_location=None, background_fill_color="black")
-        byplot.vbar(x='date', source=source, width=width, bottom=0, top=1, color=mapper)
-        # byplot.vbar(x='date', source=source, width=width, bottom=0, top='count', color=mapper)
-        byplot.xaxis.visible = False
-        byplot.xgrid.visible = False
-        byplot.yaxis.visible = False
-        byplot.ygrid.visible = False
-        byplot.add_tools(HoverTool(tooltips=[("# of values", "@count")], mode="mouse"))
-
-        # Style the plot
-        mainplot.title.text_font_size = "14pt"
-        mainplot.xaxis.axis_label_text_font_size = "14pt"
-        mainplot.xaxis.formatter = DatetimeTickFormatter(days=["%d %b %Y"], months=["%d %b %Y"], years=["%d %b %Y"])
-        mainplot.yaxis.axis_label_text_font_size = "14pt"
-
-        script, div = components(column(byplot, mainplot), wrap_script=False)
-        img = {'script': script, 'div': div}
         if use_redis:
             r.set("preview_{}".format('b' + id), img)
 
     return img
-
