2f1687932d84ba4314c96a1df7644efeb9da7ce2
MarcusStrobl <marcus.strobl@kit.edu>
2021-04-12 12:21:53 +0200

    fix redirect when toggle language

--- vfwheron/views.py
+++ vfwheron/views.py
@@ -21,7 +21,8 @@ from django.views.generic import TemplateView
 from django.contrib import messages
 from future.builtins import isinstance
 
-from heron.settings import LOCAL_GEOSERVER, DEBUG
+from author_manage.views import MyResourcesView
+from heron.settings import LOCAL_GEOSERVER, DEBUG, DEMO_VAR
 
 from vfwheron.geoserver_layer import create_layer, get_layer, delete_layer, test_geoserver_env
 from vfwheron.previewplot import get_preview, get_fullres_plot
@@ -110,6 +111,9 @@ class HomeView(TemplateView):
 
     # TODO: Test with users if this makes any sense
     def __set_layer_name(self):
+        """
+        Set name for layer in geoserver according to username or as admin_layer.
+        """
         if self.request.user.is_authenticated:
             if self.request.user.is_superuser:
                 self.data_layer = 'admin_layer'
@@ -117,7 +121,13 @@ class HomeView(TemplateView):
                 self.data_layer = expressive_layer_name(self.request.user)
 
     # Put here everything you need at startup and for refresh of 'Home'
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs: object):
+        """
+        Collect data needed for startup of V-FOR-WaTer Portal home.
+
+        :param kwargs:
+        :return:
+        """
         self.__set_layer_name()
         # get_dataset(self, **kwargs)
 
@@ -462,7 +472,7 @@ class ToggleLanguageView(View):
                 request.session[translation.LANGUAGE_SESSION_KEY] = 'en-gb'
         logger.debug('new language: {}'.format(translation.get_language()))
         logger.debug('translation test: {}'.format(translation.gettext("help")))
-        return redirect('/')
+        return redirect(DEMO_VAR + '/')
 
 
 class FailedLoginView(View):
