a595add819d7fa6f8a316fa0dc78bd3cd0fb6097
MarcusStrobl <marcus.strobl@kit.edu>
2021-10-12 16:53:33 +0200

    fix "get dataset" button from preview more

--- vfwheron/static/vfwheron/js/vfw.js
+++ vfwheron/static/vfwheron/js/vfw.js
@@ -655,9 +655,9 @@ function moreInfoModal(id) {
                 '</tr>';
         }
         if (properties.Embargo == 'No'){
-            metaText += '<tr><td colspan = "2">' + storeBtn(id, "False") + '</td></tr>'
+            metaText += '<tr><td colspan = "2">' + storeBtn(properties.id, "False") + '</td></tr>'
         } else {
-            metaText += '<tr><td colspan = "2">' + storeBtn(id, "True") + '</td></tr>'
+            metaText += '<tr><td colspan = "2">' + storeBtn(properties.id, "True") + '</td></tr>'
         }
         document.getElementById('mod_dat_inf').innerHTML = metaText + '</table>';
         showDataInfo(properties);
