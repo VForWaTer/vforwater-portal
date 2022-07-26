77f0adf8c463e0ba66c8daae3f4049be6814d7c2
MarcusStrobl <marcus.strobl@kit.edu>
2022-07-26 11:56:48 +0200

    Add more services to VFW status page and make it findable in the admin menu

--- vfw_status/views.py
+++ vfw_status/views.py
@@ -10,6 +10,7 @@ def home(request):
     Dummy page for Self Monitor tool.
     """
     services = {}
+    rs = redis.Redis("localhost")
 
     def get_status(url, secret):
         checked_service = {'runs': False}
@@ -26,9 +27,29 @@ def home(request):
 
         return checked_service
 
-    services['DEBUG is off'] = {'runs': not DEBUG, 'code': '¡666!' if DEBUG else '',
+    # local 'services'
+    services['DEBUG is off'] = {'runs': not DEBUG,
                                 'url': 'Make sure DEBUG is only on when needed'}
+    db_connection = django.db.connection.ensure_connection()
+    services['Database connection'] = {'runs': True} if db_connection is None else {'runs': False}
+    services['Database connection']['url'] = 'django.db.connection.ensure_connection()'
 
+    try:
+        rs.client_list()
+        connected = True
+    except redis.ConnectionError as e:
+        connected = False
+        print('Can not connect to Redis: ', e)
+    services['Redis connection'] = {'runs': connected, 'url': 'redis.Redis("localhost").client_list()'}
+    try:
+        rs.ping()
+        connected = True
+    except Exception as e:
+        connected = False
+        print('Can not ping Redis: ', e)
+    services['Redis running'] = {'runs': connected, 'url': 'redis.Redis("localhost").ping()'}
+
+    # url tests
     services['GeoServer'] = get_status("{}/rest/about/status.json".format(LOCAL_GEOSERVER), SECRET_GEOSERVER)
     services['Map'] = get_status("{}/osm/{z}/{x}/{y}.png".format(MAP_SERVER, x=0, y=0, z=0), False)
 
@@ -36,8 +57,8 @@ def home(request):
     for pywps in db_pywps:
         services[pywps['name']] = get_status("{}?service=WPS&request=GetCapabilities".format(pywps['endpoint']),
                                              "'{}', '{}'".format(pywps['username'], pywps['password']))
-        # services[pywps['name'] + ' process Access'] = get_status(
-        #     "{}?service=WPS&request=DescribeProcess&version=1.0.0&identifier=flowdurationcurve".format(
-        #         pywps['endpoint']), "'{}', '{}'".format(pywps['username'], pywps['password']))
+        services[pywps['name'] + ' process Access'] = get_status(
+            "{}?service=WPS&request=DescribeProcess&version=1.0.0&identifier=flowdurationcurve".format(
+                pywps['endpoint']), "'{}', '{}'".format(pywps['username'], pywps['password']))
 
     return render(request, 'vfw_status/home.html', {'services': services})
