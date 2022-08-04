14d0413d1dceeac33a5b6bf9d4faf0adeba04d9e
MarcusStrobl <marcus.strobl@kit.edu>
2022-08-04 10:21:26 +0200

    extend data obj and plot obj

--- vfw_home/data_obj.py
+++ vfw_home/data_obj.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from django.core.exceptions import EmptyResultSet
 
@@ -12,32 +13,38 @@ class DataObject:
 
         self.has_precision = None
         self.dataframe = None
-        self.qs_cols = None
+        self.__qs_cols__ = None
         self.data_columns = None
-        self.data_qs = None
-        self.qs = None
+        self.__data_qs__ = None
         self.data_table_name = None
         self.timescale = None
-        self.df = None
         self.data_format = None
         self.webID = webID
         self.date = date
+        self.has_nan = False
 
         self.full = True
         self.ID = None
         self.source = None
-        self.qs_entry = None
-        self.general_data_qs = None
+        self.__qs_entry__ = None
+        self.__general_data_qs__ = None
         self.label = ""
+        self.__value_before_gap__ = None
 
         self.metadata = {}
 
         self.length = None
+        self.missing_data = None
         self.timestep_label = ""
-        self.timestep_size = 0
+
+        self.range_dict = {
+            "x_min": None,
+            "x_max": None,
+            "y_min": None,
+            "y_max": None
+        }
 
         self.__set_vars__()
-        print('data_table_name: ', self.data_table_name)
 
     def __str__(self):
         return self.label
@@ -47,36 +54,39 @@ class DataObject:
         if self.webID[0:2] == 'db':
             self.ID = self.webID[2:]
             self.source = 'db'
-            self.qs_entry = Entries.objects.filter(id=self.ID)
-            self.data_table_name = self.qs_entry.values_list('datasource__path', flat=True)[0]
-            self.set_general_data_qs()
+            self.__qs_entry__ = Entries.objects.filter(id=self.ID)
+            self.data_table_name = self.__qs_entry__.values_list('datasource__path', flat=True)[0]
+            self.__set_general_data_qs__()
             self.length = self.db_data_length()
-
-            label = self.qs_entry.values_list('variable__name', 'variable__symbol', 'variable__unit__symbol')
+            label = self.__qs_entry__.values_list('variable__name', 'variable__symbol', 'variable__unit__symbol')
             self.label = self.format_label(label[0][0], label[0][1], label[0][2])
-            self.set_data_qs()
-            self.get_db_data()
-            self.set_precision()
+            self.__set_data_qs__()
+            self.__get_db_data__()
+            self.__set_precision__()
             self.has_precision = False
+            self.__set_timescale__()
+            self.__get_gap_pos__()
+            if len(self.__value_before_gap__) > 0:
+                self.__fill_data_gaps__()
 
         elif self.webID[0:3] == 'wps':
             self.ID = self.webID[3:]
             self.source = 'wps'
 
-    def set_precision(self):
+    def __set_precision__(self):
         if not self.dataframe['precision'].isnull().values.any():
             self.has_precision = True
 
-    def set_data_qs(self):
-        qs = self.general_data_qs
+    def __set_data_qs__(self):
+        qs = self.__general_data_qs__
         if not self.full:
-            qs = self.general_data_qs[:max_size_preview_plot]
+            qs = self.__general_data_qs__[:max_size_preview_plot]
         if self.data_table_name == 'timeseries_1d':
             self.data_columns = ['tstamp', 'value', 'precision']
-            self.qs_cols = [self.data_table_name + '__' + i for i in self.data_columns]
-            self.data_qs = qs.values(*self.qs_cols)  # .explain(verbose=True)
+            self.__qs_cols__ = [self.data_table_name + '__' + i for i in self.data_columns]
+            self.__data_qs__ = qs.values(*self.__qs_cols__)  # .explain(verbose=True)
 
-    def get_db_data(self):
+    def __get_db_data__(self):
         if self.label.find('direction') != -1:
             self.timestep_label = 'week'  # time interval used to plot, choose 'year', 'month', 'week' or 'day'
             # if full_res is False:
@@ -85,36 +95,35 @@ class DataObject:
         elif self.label.find('windspeed') != -1:
             print('its SPEED!! _________________')
         else:
-            print('______ else')
-            df = pd.DataFrame(list(self.data_qs))
-            self.dataframe = df.rename(columns=dict(zip(self.qs_cols, self.data_columns)), errors="raise")
+            df = pd.DataFrame(list(self.__data_qs__))
+            self.dataframe = df.rename(columns=dict(zip(self.__qs_cols__, self.data_columns)), errors="raise")
 
-    def timescale_from_data(self):
-        stepsize = []
+    def __timescale_from_data__(self):
+        steplist = []
         steps = 1
-        checklength = 11
+        testlength = self.dataframe.shape[0] if self.dataframe.shape[0] <= 11 else 11
 
-        if self.df.shape[0] <= checklength + 1:
-            checklength = self.df.shape[0] - 1
+        if self.dataframe.shape[0] <= testlength + 1:
+            testlength = self.dataframe.shape[0] - 1
 
-        if 'tstamp' in self.df:
-            relcol = self.df['tstamp']
-        elif self.df.index.name == 'tstamp':
-            relcol = self.df.index
+        if 'tstamp' in self.dataframe:
+            relcol = self.dataframe['tstamp']
+        elif self.dataframe.index.name == 'tstamp':
+            relcol = self.dataframe.index
 
-        while steps < checklength:
-            stepsize.append(relcol[steps + 1] - relcol[steps])
+        while steps < testlength:
+            steplist.append(relcol[steps + 1] - relcol[steps])
             steps += 1
-        self.timescale = min(stepsize)
+        self.timescale = min(steplist)
 
-    def set_timescale(self):
-        timescale = self.qs_entry.values_list('datasource__temporal_scale__resolution')[0][0]
+    def __set_timescale__(self):
+        timescale = self.__qs_entry__.values_list('datasource__temporal_scale__resolution')[0][0]
         if timescale:
             self.timescale = pd.to_timedelta(timescale)
         else:
-            self.timescale_from_data(self)
+            self.__timescale_from_data__(self)
 
-    def set_general_data_qs(self):
+    def __set_general_data_qs__(self):
         """
         Django QuerySet to access data (with respect to data_table_name and date).
         Use qs_data to get value or values_list of interest.
@@ -128,16 +137,16 @@ class DataObject:
             query_path[self.data_table_name + '__tstamp__gte'] = self.date[0]
             query_path[self.data_table_name + '__tstamp__lte'] = self.date[1]
 
-        self.general_data_qs = Entries.objects.filter(**query_path)
+        self.__general_data_qs__ = Entries.objects.filter(**query_path)
 
     def db_data_length(self):
         """
         Get length of Dataset stored in the database.
         If size is greater than allowd in settings.max_size_preview_plot the "full" flag is set to False
         """
-        self.length = self.general_data_qs.count()
+        self.length = self.__general_data_qs__.count()
         if self.length == 0:  # if not qs.exists():
-            print('Problems with query_path: ', self.general_data_qs)
+            print('Problems with query_path: ', self.__general_data_qs__)
             raise EmptyResultSet('Got no data in data_tools.is_data_short for id={}'.format(self.ID))
 
         if self.length > max_size_preview_plot:
@@ -152,3 +161,121 @@ class DataObject:
         superscript_label = unit_symbol.replace("^", "").translate(sup)
         return "{} ({}) [{}]".format(name, symbol, superscript_label)
 
+    def __set_range__(self):
+        """
+        TODO: shouldn't I use min/max of sums?
+        """
+        if self.has_precision and 'value' in self.dataframe.columns:
+            self.range_dict['y_max'] = self.dataframe['value'] + self.dataframe['precision']
+            self.range_dict['y_min'] = self.dataframe['value'] - self.dataframe['precision']
+        elif self.has_precision and 'avg' in self.dataframe.columns:
+            self.range_dict['y_max'] = self.dataframe['avg'] + self.dataframe['precmax']
+            # self.range_dict['y_min'] = self.dataframe['acg'] - self.dataframe['precmax']
+            self.range_dict['y_min'] = self.dataframe['avg'] - self.dataframe['precmax']
+            self.range_dict['y_max_avg'] = self.dataframe['avg'] + self.dataframe['precavg']
+            # self.range_dict['y_min_avg'] = self.dataframe['acg'] - self.dataframe['precavg']
+            self.range_dict['y_min_avg'] = self.dataframe['avg'] - self.dataframe['precavg']
+        elif 'value' in self.dataframe.columns:
+            self.range_dict['y_max'] = max(self.dataframe['value'])
+            self.range_dict['y_min'] = min(self.dataframe['value'])
+        else:
+            print('WARNING: there is a unknown dataset to convert precision in precision to minmax.')
+
+    def __get_gap_pos__(self):
+        """
+        Iterate over a dataset and find all positions where the distance between two rows is greater than the given scale.
+        """
+        index = 'tstamp'
+        val_beforeGap = []
+        # use datetime as index and request the list of datetimes your looking for
+        starttime = self.dataframe[index][0]
+        endtime = self.dataframe.iloc[-1][index]
+        # create a perfect DataFrame without gaps
+        perfect_frame = pd.DataFrame(pd.date_range(start=starttime, end=endtime, freq=self.timescale), columns=['tstamp'])
+        # fill the empty frame without gaps with the data from original frame
+        combined_dataframes = pd.merge(perfect_frame, self.dataframe, how="outer", on="tstamp")
+        # get rows without values
+        try:
+            empty_list = combined_dataframes.loc[combined_dataframes['value'].isna()].index.values
+        except Exception as e:
+            print('in get gab position: ', e)
+            empty_list = combined_dataframes.loc[combined_dataframes['data'].isna()].index.values
+
+        # use two shifted lists for comparison to find the first value of a gap
+        shifted_list = np.append(0, empty_list)
+        empty_list = np.append(empty_list, 0)
+        diff_list = np.subtract(empty_list, shifted_list)
+
+        # get indices of the perfect dataframe (without gaps)
+        perfectframe_gaps = empty_list[diff_list > 1] - 1
+        # get list of dates just before the gap
+        gaps_date_list = combined_dataframes.loc[perfectframe_gaps.tolist()][index].tolist()
+
+        # make a list of the indices (from the original dataframe) of the position before the gaps
+        for i in gaps_date_list:
+            val_beforeGapRow = self.dataframe.loc[self.dataframe[index] == i]
+            val_beforeGap.append(val_beforeGapRow.index[0])
+        self.__value_before_gap__ = val_beforeGap
+
+    def __fill_data_gaps__(self):
+        """
+        Fill gaps in datasets and prepare for plot
+        """
+        self.has_nan = True
+        defect_x = []
+        defect_y = []
+        if 'data' in self.data_columns:
+            col = 'data'
+        elif 'value' in self.data_columns:
+            col = 'value'
+        # if 'precision' in df.columns and df['precision'].sum() > 0:  # if preview with average, min, max  values
+        if 'avg' in self.dataframe.columns:  # if dataset with average, min, max  values
+            for pos in self.__value_before_gap__[::-1]:
+                # TODO: change this code to pandas df.
+                print('TODO: change this code to pandas df.')
+                self.dataframe[col][0] = self.dataframe[col][0][: pos + 1] + \
+                                     (self.dataframe[col][0][pos] + self.timescale,
+                                      self.dataframe[col][0][pos + 1] - self.timescale,) + \
+                                     self.dataframe[col][0][pos + 1:]
+                self.dataframe[col][1] = self.dataframe[col][1][: pos + 1] + (float('nan'), float('nan'),) + \
+                                     self.dataframe[col][1][pos + 1:]
+                bandbef = (self.dataframe[col][2][pos] + self.dataframe[col][3][pos]) / 2
+                bandaft = (self.dataframe[col][2][pos + 1] + self.dataframe[col][3][pos + 1]) / 2
+                self.dataframe[col][2] = self.dataframe[col][2][: pos + 1] + (bandbef, bandaft,) + self.dataframe[col][2][
+                                                                                           pos + 1:]
+                self.dataframe[col][3] = self.dataframe[col][3][: pos + 1] + (bandbef, bandaft,) + self.dataframe[col][3][
+                                                                                           pos + 1:]
+                self.dataframe[col][4] = self.dataframe[col][4][: pos + 1] + (0, 0,) + self.dataframe[col][4][pos + 1:]
+                # white_line = white_line[: pos + 1] + (bandbef, bandaft,) + white_line[pos + 1:]
+                defect_x.extend([self.dataframe[col][0][pos] - self.timescale, self.dataframe[col][0][pos],
+                                 self.dataframe[col][0][pos + 3], self.dataframe[col][0][pos] + self.timescale])
+                defect_y.extend([float('nan'), self.dataframe[col][1][pos],
+                                 self.dataframe[col][1][pos + 3], float('nan'), ])
+            source = pd.DataFrame({'date': self.dataframe[col][0], 'y': self.dataframe[col][1],
+                                   'ymin': self.dataframe[col][2], 'ymax': self.dataframe[col][3],
+                                   'count': self.dataframe[col][4]})
+            self.missing_data = pd.DataFrame({'tstamp': defect_x, 'value': defect_y})
+        else:  # if full dataset, without average, min, max values
+            # copy random rows which happen to be the first two
+            empty_rows = self.dataframe.loc[1:2].copy()
+            # set all columns except tstamp to nan
+            empty_rows.loc[:, empty_rows.columns != 'tstamp'] = float('nan')
+            for pos in self.__value_before_gap__[::-1]:
+                # set correct tstamp to new rows
+                empty_rows['tstamp'] = self.dataframe['tstamp'][pos] + self.timescale, self.dataframe['tstamp'][pos + 1] - self.timescale
+                # insert new rows with float index positions
+                self.dataframe.loc[pos + 0.3] = empty_rows.loc[1]
+                # df.loc[pos+0.3] = df['tstamp'][pos] + scale, float('nan'), float('nan')
+                self.dataframe.loc[pos + 0.6] = empty_rows.loc[2]
+                # df.loc[pos+0.6] = df['tstamp'][pos + 1] - scale, float('nan'), float('nan')
+                defect_x.extend([self.dataframe['tstamp'][pos] - self.timescale, self.dataframe['tstamp'][pos],
+                                 self.dataframe['tstamp'][pos + 1], self.dataframe['tstamp'][pos + 1] + self.timescale])
+                if self.data_format == '3D':
+                    defect_y.extend([float('nan'), float('nan'), float('nan'), float('nan'), ])
+                elif 'value' in self.dataframe.columns:
+                    defect_y.extend([float('nan'), self.dataframe['value'][pos], self.dataframe['value'][pos + 1], float('nan'), ])
+                elif 'data' in self.dataframe.columns:
+                    defect_y.extend([float('nan'), self.dataframe['data'][pos], self.dataframe['data'][pos + 1], float('nan'), ])
+            # reset the index to integer
+            self.dataframe = self.dataframe.sort_index().reset_index(drop=True)
+            self.missing_data = pd.DataFrame({'tstamp': defect_x, 'value': defect_y})
--- vfw_home/plot_obj.py
+++ vfw_home/plot_obj.py
@@ -1,32 +1,113 @@
+from bokeh.embed import components
+from bokeh.layouts import column
+from bokeh.models import HoverTool, DatetimeTickFormatter, ColumnDataSource, BoxAnnotation, Whisker
+from bokeh.plotting import figure
+from django.utils.translation import gettext
+
+from heron.settings import max_size_preview_plot
+
+
 class PlotObject:
     """
     Class calculates parameters accroding the parameters.
     """
 
-    def __init__(self, webID="", data=None, date="", full_res=False, plot_size=[700, 500]):
-        self.webID = webID
-        self.data = data
-        self.date = date
-        self.full_res = full_res
+    def __init__(self, data=None, plot_size=[700, 500]):
+        self.dataObj = data
+        self.source = ColumnDataSource(self.dataObj.dataframe)
         self.plot_size = plot_size
 
         self.dbinput = None
         self.wpsinput = None
         self.title = ""
+        self.mainplot = None
+        self.x_axis_type = None
+        self.x_axis_label = "TODO: Find a proper label in your Data!"
+        self.script = None
+        self.div = None
+        self.get_plot = None
 
-        # Plot ranges
-        self.range_dict = {
-            "x_min": None,
-            "x_max": None,
-            "y_min": None,
-            "y_max": None
-        }
+        self.__set_values__()
 
-    def get_data(self):
-        if self.webID[0:2] == "db":
-            raise
+    def __set_values__(self):
 
-    def preprocess_data(self):
-        raise
+        if self.dataObj.full:
+            self.title = ''
+        else:
+            self.title = gettext("Showing only latest {0} datapoints.").format(str(max_size_preview_plot))
 
-    # def
+        if self.dataObj.label.find('direction') != -1:
+            print('we need a direction plot ____________________')
+        elif self.dataObj.label.find('windspeed') != -1:
+            print('we need a wind SPEED!! plot _________________')
+        elif self.dataObj.data_format == '3D':
+            print('we need a 3D plot ________________-')
+        elif self.dataObj.data_table_name.find('timeseries') != -1:
+            self.x_axis_type = "datetime"  # use "mercator" for geodata!
+            self.x_axis_label = "Time"
+            self.x_col = 'tstamp'
+            self.y_col = 'value'
+
+            self.__set_mainplot__()
+            self.__add_data__()
+            self.__add_timeseries_specifics__()
+            self.__style_plot__()
+            if self.dataObj.has_nan:
+                self.__show_nan__()
+
+            if self.dataObj.has_precision:
+                self.__show_precision__()
+        else:
+            print('we need a standard plot!_______________')
+
+        self.__create_plot__()
+        self.get_plot = {'script': self.script, 'div': self.div}
+
+    def __set_mainplot__(self):
+        self.mainplot = figure(x_axis_label=self.x_axis_label, x_axis_type=self.x_axis_type,
+                               y_axis_label=self.dataObj.label,
+                               title=self.title,
+                               # sizing_mode='stretch_both',
+                               plot_width=self.plot_size[0], plot_height=int(self.plot_size[1] * 0.9),
+                               toolbar_location="above", tools="pan,wheel_zoom,box_zoom,reset, save",
+                               active_drag="box_zoom")
+
+    def __add_data__(self):
+        # plot value line
+        self.mainplot.line(x=self.x_col, y=self.y_col, source=self.source,
+                           line_width=3)  # , legend_label="measured values")
+
+    def __show_nan__(self):
+        self.mainplot.line(x=self.x_col, y=self.y_col, line_width=3, line_color='red', line_cap='round',
+                           legend_label=gettext('Missing values'), source=ColumnDataSource(self.dataObj.missing_data),
+                           # visible=False
+                           )
+        self.mainplot.legend.click_policy = "hide"
+        mis_list = [*range(0, len(self.dataObj.missing_data), 4)]
+        for i in mis_list:
+            box = BoxAnnotation(left=self.dataObj.missing_data[self.x_col][i + 1],
+                                right=self.dataObj.missing_data[self.x_col][i + 2],
+                                fill_alpha=0.2, fill_color='red')
+            self.mainplot.add_layout(box)
+
+    def __show_precision__(self):
+        self.mainplot.add_layout(Whisker(source=self.source, base="tstamp", upper="upper", lower="lower",
+                                         line_width=0.5))
+
+    def __add_timeseries_specifics__(self):
+
+        self.mainplot.add_tools(HoverTool(tooltips=[(gettext("Value"), "@value"),
+                                                    (gettext("Time"), "@tstamp{%T %Z}"),
+                                                    (gettext("Date"), "@tstamp{%d %b %Y}")],
+                                          formatters={"@tstamp": "datetime"}, mode="mouse"))
+        self.mainplot.xaxis.formatter = DatetimeTickFormatter(days=["%d %b %Y"], months=["%d %b %Y"], years=["%d %b %Y"])
+
+    def __style_plot__(self):
+        self.mainplot.title.text_font_size = "14pt"
+        self.mainplot.xaxis.axis_label_text_font_size = "14pt"
+        self.mainplot.yaxis.axis_label_text_font_size = "14pt"
+        # self.mainplot.BoxAnnotation(top=80, fill_alpha=0.1, fill_color='red')
+
+    def __create_plot__(self):
+        self.script, self.div = components(column(self.mainplot, sizing_mode="scale_both"), wrap_script=False)
+
