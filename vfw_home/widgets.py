bf885783bd090b891064b2d8c50a8c187fef42f8
MarcusStrobl <marcus.strobl@kit.edu>
2023-06-14 16:03:26 +0200

    Improve range slider widgets and move widget html and js code to separate template files

--- vfw_home/widgets.py
+++ vfw_home/widgets.py
@@ -23,9 +23,14 @@ class Slider(forms.HiddenInput):
     def render(self, name, value, attrs=None, renderer=None):
         s = super(Slider, self).render(name, value, attrs)
         self.elem_id = re.findall(r'id_([A-Za-z0-9_\./\\-]*)"', s)[0]
-        template = Template("""<div class="slidecontainer">
-        <input type="range" min={{ minimum }} max={{ maximum }} value={{ minimum }} class="slider"
-        id='valueslider_{{ id }}'><p>{{ id }}: <span id='slidOut_{{ id }}'></span></p></div>""")
+        template = Template(
+            """
+            <div class="slidecontainer">
+            <input type="range" min={{ minimum }} max={{ maximum }} value={{ minimum }} class="slider"
+            id='valueslider_{{ id }}'>
+            <p>{{ id }}: <span id='slidOut_{{ id }}'></span>
+            </p>
+            </div>""")
         html = template.render(Context({'id': self.elem_id, 'minimum': self.minimum, 'maximum': self.maximum}))
         return mark_safe(s + html)
 
@@ -76,41 +81,15 @@ class DateRangeSlider(forms.DateInput):
     def render(self, name, value, attrs=None, renderer=None):
         s = super(DateRangeSlider, self).render(name, value, attrs)
         self.elem_id = re.findall(r'id_([A-Za-z0-9_\./\\-]*)"', s)[0]
-        # TODO: implement behaviour when mouse leaves slider without buttonup event
-        # <div onmouseup='""" + self.onchange + """' onmouseleave='""" + self.onchange + """'
-        html = """
-        <div onmouseup='""" + self.onchange + """'
-        id="slider-date-range-""" + self.elem_id + """"></div>
-        <script>
-        $('#id_""" + self.elem_id + """').attr("readonly", true)
-        $( "#slider-date-range-""" + self.elem_id + """" ).slider({
-        range: true,
-        min: new Date('""" + self.minimum + """').getTime(),
-        max: new Date('""" + self.maximum + """').getTime(),
-        step: """ + self.step + """,
-        values: [ new Date('""" + self.minimum + """'),new Date('""" + self.maximum + """') ],
-        slide: function( event, ui ) {
-          let minDate = new Date(ui.values[0]);
-          let maxDate = new Date(ui.values[1]);
-          $( "#id_""" + self.elem_id + """" )
-          .val(" """ + self.elem_name + """ "+ minDate.toLocaleDateString() + " - " + maxDate.toLocaleDateString() );
-          $( "#id_""" + self.elem_id + """" )
-          .data("values", [minDate.toISOString().split('T')[0], maxDate.toISOString().split('T')[0]]);
-          // if (event.keyCode >= 37 && event.keyCode <= 40) {
-          // console.log(event.keyCode)
-          if (event.keyCode == 13) {
-          """ + self.onchange + """
-          }
-        }
-        });
-        // initial values of box:
-        let minStart = new Date($( "#slider-date-range-""" + self.elem_id + """" ).slider("values", 0))
-        .toLocaleDateString()
-        let maxStart = new Date($( "#slider-date-range-""" + self.elem_id + """" ).slider("values", 1))
-        .toLocaleDateString()
-        $( "#id_""" + self.elem_id + """" ).val(" """ + self.elem_name + """ " + minStart + " - " + maxStart);
-        </script>
-        """
+
+        html = loader.get_template('vfw_home/daterangeslider_widget.html')\
+            .render(context={'onchange': self.onchange,
+                             'elem_id': self.elem_id,
+                             'elem_name': self.elem_name,
+                             'minimum': self.minimum,
+                             'maximum': self.maximum,
+                             'step': self.step,
+                             })
         return mark_safe(s + html)
 
 
@@ -126,28 +105,14 @@ class DateTimeRangeSlider(forms.DateTimeInput):
     def render(self, name, value, attrs=None, renderer=None):
         s = super(DateTimeRangeSlider, self).render(name, value, attrs)
         self.elem_id = re.findall(r'id_([A-Za-z0-9_\./\\-]*)"', s)[0]
-        html = """<div onmouseup='""" + self.onchange + """' onmouseleave='""" + self.onchange + """' id="slider-d-t-range-""" + self.elem_id + """"></div>
-        <script>
-        """ + self.onchange + """
-        $('#id_""" + self.elem_id + """').attr("readonly", true)
-        $( "#slider-d-t-range-""" + self.elem_id + """" ).slider({
-        range: true,
-        min: new Date('""" + self.minimum + """').getTime(),
-        max: new Date('""" + self.maximum + """').getTime(),
-        step: """ + self.step + """,
-        values: [new Date('""" + self.minimum + """'), new Date('""" + self.maximum + """')],
-        slide: function( event, ui ) {
-          let minDate = new Date(ui.values[0]).toLocaleString();
-          let maxDate = new Date(ui.values[1]).toLocaleString();
-          $( "#id_""" + self.elem_id + """" ).val(" """ + self.elem_name + """ "+ minDate + " - " + maxDate );
-        }
-        });
-        // initial values of box:
-        let minStart = new Date($( "#slider-d-t-range-""" + self.elem_id + """" ).slider("values", 0)).toLocaleString()
-        let maxStart = new Date($( "#slider-d-t-range-""" + self.elem_id + """" ).slider("values", 1)).toLocaleString()
-        $( "#id_""" + self.elem_id + """" ).val(" """ + self.elem_name + """ " + minStart + " - " + maxStart);
-        </script>
-        """
+        html = loader.get_template('vfw_home/datetimerangeslider_widget.html')\
+            .render(context={'onchange': self.onchange,
+                             'elem_id': self.elem_id,
+                             'elem_name': self.elem_name,
+                             'minimum': self.minimum,
+                             'maximum': self.maximum,
+                             'step': self.step,
+                             })
         return mark_safe(s + html)
 
 
