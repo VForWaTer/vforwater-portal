2c5d896650bc4a67d9cc3ece1c37ff131aae31c9
or4879 <kaoutar.boussaourd@kit.edu>
2024-06-06 10:11:19 +0200

    add new modif

--- vfw_home/datatypes.py
+++
@@ -1,8 +0,0 @@
-datatypes = ['varray', 'iarray', 'array', 'vtimeseries', 'timeseries',
-             'raster', 'ndarray', 'vraster', '2darray', 'idataframe', 'vdataframe',
-             'time-dataframe', 'vtime-dataframe',
-             # outdated values:
-             'ts-aggregate', 'ts-pickle', 'ts-merge', 'aggregate',
-             'pickle', 'merge', 'merged-pickle', 'merged-ts-pickle', 'geometry'
-             ]
-basicdatatypes = ['string', 'boolean', 'float', 'integer', 'number', 'json']
