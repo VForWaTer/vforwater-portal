c9535c9f3e47c0de1620b5c4c8df0d48a3fbbf78
elnaz.azmi <elnaz.azmi@kit.edu>
2025-04-22 12:10:50 +0200

    get coordinates for the selected catchment and add it to the session storage.

--- vfw_home/static/vfw_home/js/sidebar.js
+++ vfw_home/static/vfw_home/js/sidebar.js
@@ -158,6 +158,11 @@ vfw.sidebar.addGroupaccordionToggle = function () {
  * @param {object} file - In case of an uploaded geometry one can pass the whole object. Yet used is only file.name
  */
 vfw.sidebar.addSelectStoreButton = function (file={}, source="userUpload") {
+    if (vfw.filter.coords.length == 0) {
+            vfw.filter.coords =
+                vfw.map.source.selectionSource.getFeatures()[0].getGeometry().getCoordinates();
+        }
+
     /** Data needed to create a Button in the datastore */
     const objData = {
         "name": "Select Area", "type": "geometry", "geom": vfw.filter.coords, "isGroupMember": false,
