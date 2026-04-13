907e2afb00394cfec3668b4100363208a93557aa
safabouguezzi <safa_bouguezzi@outlook.com>
2026-04-13 12:45:42 +0200

    add the tool combined_loader_simulation_evaluation

--- wps_gui/views.py
+++ wps_gui/views.py
@@ -79,7 +79,7 @@ default is accessible for everyone after log-in, the rest only for admins or on 
 """
 TOOLDICT = {  
 
-    "default": ["vforwater_loader", "dataset_profiler", "variogram", "combined_loader_whitebox"],
+    "default": ["vforwater_loader", "dataset_profiler", "variogram", "combined_loader_whitebox", "combined_loader_simulation_evaluation"],
     "short_running_debug": ["hello-world"],  # available for any user in debug mode
     "short_running": [],  # available for any user, also if not logged in
 
